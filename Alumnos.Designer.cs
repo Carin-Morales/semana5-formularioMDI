2062c7869203276d4e9cc317d9db0451dddb0c96
FaridClaros <faridclaros15@gmail.com>
2022-09-01 19:30:24 -0600

    Cambio de fondo del formulario basico de registro

--- Alumnos.Designer.cs
+++ Alumnos.Designer.cs
@@ -50,9 +50,10 @@ namespace semana5_formularioMDI
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(62, 54);
+            this.label1.Location = new System.Drawing.Point(83, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 22);
+            this.label1.Size = new System.Drawing.Size(95, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre";
             // 
@@ -60,9 +61,10 @@ namespace semana5_formularioMDI
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(62, 113);
+            this.label2.Location = new System.Drawing.Point(83, 139);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 22);
+            this.label2.Size = new System.Drawing.Size(108, 26);
             this.label2.TabIndex = 1;
             this.label2.Text = "Apellidos";
             // 
@@ -70,9 +72,10 @@ namespace semana5_formularioMDI
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(62, 233);
+            this.label4.Location = new System.Drawing.Point(83, 287);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(80, 22);
+            this.label4.Size = new System.Drawing.Size(98, 26);
             this.label4.TabIndex = 3;
             this.label4.Text = "Telefono";
             // 
@@ -80,26 +83,29 @@ namespace semana5_formularioMDI
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(62, 297);
+            this.label5.Location = new System.Drawing.Point(83, 366);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(69, 22);
+            this.label5.Size = new System.Drawing.Size(86, 26);
             this.label5.TabIndex = 4;
             this.label5.Text = "Correo";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(165, 54);
+            this.textBox1.Location = new System.Drawing.Point(220, 66);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(282, 28);
+            this.textBox1.Size = new System.Drawing.Size(375, 34);
             this.textBox1.TabIndex = 5;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(135, 372);
+            this.button1.Location = new System.Drawing.Point(180, 458);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 39);
+            this.button1.Size = new System.Drawing.Size(152, 48);
             this.button1.TabIndex = 10;
             this.button1.Text = "REGISTRARME";
             this.button1.UseVisualStyleBackColor = true;
@@ -107,9 +113,10 @@ namespace semana5_formularioMDI
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(390, 372);
+            this.button2.Location = new System.Drawing.Point(520, 458);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 39);
+            this.button2.Size = new System.Drawing.Size(159, 48);
             this.button2.TabIndex = 11;
             this.button2.Text = "CERRAR";
             this.button2.UseVisualStyleBackColor = true;
@@ -119,9 +126,10 @@ namespace semana5_formularioMDI
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(62, 183);
+            this.label3.Location = new System.Drawing.Point(83, 225);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 22);
+            this.label3.Size = new System.Drawing.Size(90, 26);
             this.label3.TabIndex = 12;
             this.label3.Text = "Celular";
             // 
@@ -129,60 +137,66 @@ namespace semana5_formularioMDI
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(231, 9);
+            this.label6.Location = new System.Drawing.Point(308, 11);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(243, 20);
+            this.label6.Size = new System.Drawing.Size(304, 25);
             this.label6.TabIndex = 15;
             this.label6.Text = "Formulario Basico de Registro";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(165, 116);
+            this.textBox2.Location = new System.Drawing.Point(220, 143);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(282, 28);
+            this.textBox2.Size = new System.Drawing.Size(375, 34);
             this.textBox2.TabIndex = 16;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(165, 177);
+            this.textBox3.Location = new System.Drawing.Point(220, 218);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(282, 28);
+            this.textBox3.Size = new System.Drawing.Size(375, 34);
             this.textBox3.TabIndex = 17;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(165, 227);
+            this.textBox4.Location = new System.Drawing.Point(220, 279);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(282, 28);
+            this.textBox4.Size = new System.Drawing.Size(375, 34);
             this.textBox4.TabIndex = 18;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(165, 297);
+            this.textBox5.Location = new System.Drawing.Point(220, 366);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(282, 28);
+            this.textBox5.Size = new System.Drawing.Size(375, 34);
             this.textBox5.TabIndex = 19;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::semana5_formularioMDI.Properties.Resources.plan_de_estudios;
-            this.pictureBox1.Location = new System.Drawing.Point(532, 54);
+            this.pictureBox1.Location = new System.Drawing.Point(709, 66);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(206, 265);
+            this.pictureBox1.Size = new System.Drawing.Size(275, 326);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 14;
             this.pictureBox1.TabStop = false;
             // 
             // Alumnos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(144)))), ((int)(((byte)(203)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
@@ -197,6 +211,7 @@ namespace semana5_formularioMDI
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Alumnos";
             this.Text = "Registro";
             this.Load += new System.EventHandler(this.Alumnos_Load);
